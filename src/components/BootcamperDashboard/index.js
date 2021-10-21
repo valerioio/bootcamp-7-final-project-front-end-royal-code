d4ad017ee66f1126d82e19e2261f0ffebe60f6ca
C-L-D <clairedoswell1995@gmail.com>
2021-10-21 15:41:18 +0100

    Changed component flow to app for conditional rendering

---
+++ src/components/BootcamperDashboard/index.js
@@ -0,0 +1,80 @@
+import List from "../List";
+import Input from "../Input";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+} from "@chakra-ui/react";
+import LogoutButton from "../LogoutButton";
+
+export default function BootcamperDashboard({
+  handleDelete,
+  addListing,
+  Listings,
+}) {
+  return (
+    <div>
+      <LogoutButton />
+      <Input onData={addListing} />
+      <List Listings={Listings} handleDelete={handleDelete} />
+      <Table variant="simple">
+        <TableCaption>Bootcamper Names</TableCaption>
+        <Thead>
+          <Tr>
+            <Th>First Name</Th>
+            <Th>Second Name</Th>
+          </Tr>{" "}
+        </Thead>
+        <Tbody>
+          <Tr>
+            <Td>name</Td>
+            <Td>name</Td>
+          </Tr>
+          <Tr>
+            <Td>name</Td>
+            <Td>name</Td>
+          </Tr>
+          <Tr>
+            <Td>name</Td>
+            <Td>name</Td>
+          </Tr>
+          <Tr>
+            <Td>name</Td>
+            <Td>name</Td>
+          </Tr>
+          <Tr>
+            <Td>name</Td>
+            <Td>name</Td>
+          </Tr>
+          <Tr>
+            <Td>name</Td>
+            <Td>name</Td>
+          </Tr>
+          <Tr>
+            <Td>name</Td>
+            <Td>name</Td>
+          </Tr>
+          <Tr>
+            <Td>name</Td>
+            <Td>name</Td>
+          </Tr>
+          <Tr>
+            <Td>name</Td>
+            <Td>name</Td>
+          </Tr>
+        </Tbody>
+        <Tfoot>
+          <Tr>
+            <Th>First Name</Th>
+            <Th>Second Name</Th>
+          </Tr>
+        </Tfoot>
+      </Table>
+    </div>
+  );
+}
