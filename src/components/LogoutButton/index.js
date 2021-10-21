9a226e7ed50a0978a0644652ee2ff183c16f1038
C-L-D <clairedoswell1995@gmail.com>
2021-10-21 15:34:53 +0100

    Altered logout button component to look like login button component

--- src/components/LogoutButton/index.js
+++ src/components/LogoutButton/index.js
@@ -5,7 +5,7 @@ const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
-    <button clonClick={() => logout({ returnTo: window.location.origin })}>
+    <button onClick={() => logout({ returnTo: window.location.origin })}>
       Log Out
     </button>
   );
