46543abea6d813f89bcfa3c40ee5f41c6cfd0608
valerio <romavalerioio@gmail.com>
2021-10-25 16:23:13 +0100

    list of pairs and groups in a beautiful container

--- src/components/CoachDashboard/index.js
+++ src/components/CoachDashboard/index.js
@@ -1,4 +1,3 @@
-import ChakraTable from "../ChakraTable";
 import Layout from "../Layout";
 import css from "./CoachDashboard.module.css";
 import "../../index.css";
@@ -6,8 +5,20 @@ import "../../index.css";
 function groupParings(bootcampers, size) {
   const groups = [];
   for (let i = 0; i < bootcampers.length; i += size) {
-    groups.push (<li> {bootcampers.slice (i, i + size).map (bootcamper => <p>{bootcamper.firstName + " " + bootcamper.lastName}</p>)} </li>)
-
+    groups.push(
+      <>
+        <p>
+          {size === 2 ? "Pair" : "Group"} {i / size + 1}
+        </p>
+        <li>
+          {" "}
+          {bootcampers.slice(i, i + size).map((bootcamper) => (
+            <p>{bootcamper.firstName + " " + bootcamper.lastName}</p>
+          ))}{" "}
+        </li>
+        <br />
+      </>
+    );
   }
   return groups;
 }
@@ -21,19 +32,31 @@ export default function CoachDashboard({
   return (
     <div>
       <Layout>
-        <div className={`${css.container} container`}>
-          <h1 className={css.title}>Bootcamper pairings</h1>
-          <ul  className={css.list}> {groupParings(bootcampers,2)}</ul>
-        </div>
-        <div className={`${css.container} container`}>
-          <h1 className={css.title}>Bootcamper groups of 4</h1>
-          <ul  className={css.list}>
-          {groupParings(bootcampers,4)}
-          </ul>
-        </div>
-        <div className={`${css.container} container`}>
-          <h1 className={css.title}>Bootcamper groups of 8</h1>
-          <ul  className={css.list} > {groupParings(bootcampers, 8)}</ul>
+        <div className={css.dashboard}>
+          <div className={`container`}>
+            <h1 className={css.title}>Bootcamper pairings</h1>
+            <ul className={css.list}> {groupParings(bootcampers, 2)}</ul>
+          </div>
+          <div className={`container`}>
+            <h1 className={css.title}>Bootcamper groups of 4</h1>
+            <ul className={css.list}>{groupParings(bootcampers, 4)}</ul>
+          </div>
+          <div className={`container`}>
+            <h1 className={css.title}>Bootcamper groups of 8</h1>
+            <ul className={css.list}> {groupParings(bootcampers, 8)}</ul>
+          </div>
+          <div className={`container`}>
+            <h1 className={css.title}>Bootcamper groups of 8</h1>
+            <ul className={css.list}> {groupParings(bootcampers, 8)}</ul>
+          </div>
+          <div className={`container`}>
+            <h1 className={css.title}>Bootcamper groups of 8</h1>
+            <ul className={css.list}> {groupParings(bootcampers, 8)}</ul>
+          </div>
+          <div className={`container`}>
+            <h1 className={css.title}>Bootcamper groups of 8</h1>
+            <ul className={css.list}> {groupParings(bootcampers, 8)}</ul>
+          </div>
         </div>
       </Layout>
     </div>
