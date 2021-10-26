e4de5886fad07c5e9f3c4a2e397dfaef5d0f00ee
valerio <romavalerioio@gmail.com>
2021-10-26 12:05:56 +0100

    begin timeline component

--- src/components/Timeline/index.js
+++ src/components/Timeline/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import css from "./Timeline.module.css"
+import css from "./Timeline.module.css";
 
 /* plan
 import react 
@@ -10,93 +10,52 @@ return = ul with jouneryData.map that will give us a contain (divs,h2, p)
 
 */
 
-export default function Timeline(){
-    return {    
-
-
-  <section id="timeline-wrapper">
-    <div class="container-fluid">
-      <div class="row">
-        <div class="timeline-top">
-          <div class="top-year">2018</span></div>
-
-        </div>
-        <div class="timeline-block">
-          <div class="timeline-events">
-            <br>
-            <div class="event l-event col-md-6 col-sm-6 col-xs-8 "><span class="thumb fa fa-codepen"></span>
-              <div class=" event-body">
-                <div class="person-image pull-left "><img src="https://cdn1.iconfinder.com/data/icons/user-pictures/100/male3-512.png" alt="person" /></div>
-<div class="event-content">
-<h5 class="text-primary text-left">IT-PLUS programming solution </h5>
-<span class="text-muted text-left" style="display:block; margin: 0"><small>19 October 2016</small></span>
-<br><br>
-                <blockquote class="text-muted text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ipsum voluptates sapiente beatae non praesentium vitae dolorum qui, reprehenderit harum <br>
-<cite class="text-muted text-right text-bold">- Alex Martin</cite>
-</blockquote>
-
-</div>
-              </div> <!-- end of event body -->
-<div class="clear-fix"></div>
-            </div><!-- end of left event -->
-            <div class="row"></div>
-            <div class="event r-event col-md-6 col-sm-6 col-xs-8 "><span class="thumb fa fa-facebook"></span>
-              <div class=" event-body">
-                <div class="person-image pull-left "><img src="https://cdn1.iconfinder.com/data/icons/user-pictures/100/male3-512.png" alt="person" /></div>
-<div class="event-content">
-<h5 class="text-primary text-left">IT-PLUS programming solution </h5>
-<span class="text-muted text-left" style="display:block; margin: 0"><small>19 October 2016</small></span>
-<br><br>
-                <blockquote class="text-muted text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ipsum voluptates sapiente beatae non praesentium vitae dolorum qui, reprehenderit harum <br>
-<cite class="text-muted text-right text-bold">- Alex Martin</cite>
-</blockquote>
-
-</div>
+export default function Timeline({ data }) {
+  const weeks = Array.from(
+    data.reduce((weeks, { week }) => weeks.add(week), new Set())
+  ).sort((a, b) => a - b);
+  return (
+    <>{weeks.map()}
+      {/* begin week block */}
+      <section id="timeline-wrapper">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="timeline-top">
+              <div class="top-year">2018</div>
+              <div class="timeline-block">
+                <div class="timeline-events">
+                  <br />
+                  {/* begin info and resources */}
+                  <div
+                    className={`${css.eventContainer} ${css.rEventContainer}`}
+                  >
+                    <span className={css.smallCircle}></span>
+                    <div className={css.container}>
+                      <div className={css.eventContent}>
+                        <h5 className={`${css.topicText} ${css.textLeft}`}>
+                          TOPIC
+                        </h5>
+                        <br />
+                        <br />
+                        <blockquote
+                          className={`${css.mainText} ${css.textLeft}`}
+                        >
+                          Lorem ipsum dolor sit amet, consectetur adipisicing
+                          elit. Quaerat ipsum voluptates sapiente beatae non
+                          praesentium vitae dolorum qui, reprehenderit harum{" "}
+                          <br />
+                        </blockquote>
+                      </div>
+                    </div>
+                  </div>
+                  {/* end info and resources */}
+                </div>
               </div>
-            </div><!-- end of right event <-->
-              <div class="clearfix"></div>
-            </-->
-            <div class="row"></div>
-            <div class="event l-event col-md-6 col-sm-6 col-xs-8 "><span class="thumb fa fa-twitter"></span>
-              <div class=" event-body">
-                <div class="person-image pull-left "><img src="https://cdn1.iconfinder.com/data/icons/user-pictures/100/male3-512.png" alt="person" /></div>
-<div class="event-content">
-<h5 class="text-primary text-left">IT-PLUS programming solution </h5>
-<span class="text-muted text-left" style="display:block; margin: 0"><small>19 October 2016</small></span>
-<br><br>
-                <blockquote class="text-muted text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ipsum voluptates sapiente beatae non praesentium vitae dolorum qui, reprehenderit harum <br>
-<cite class="text-muted text-right text-bold">- Alex Martin</cite>
-</blockquote>
-                </div>
-</div>
-            </div><!-- end of left event <-->
-              <div class="clearfix"></div>
-            
-            <div class="row"></div>
-            <div class="event r-event col-md-6 col-sm-6 col-xs-8 ">
-              <span class="thumb fa fa-codepen"></span>
-             <div class=" event-body">
-                <div class="person-image pull-left "><img src="https://cdn1.iconfinder.com/data/icons/user-pictures/100/male3-512.png" alt="person" /></div>
-<div class="event-content">
-<h5 class="text-primary text-left">IT-PLUS programming solution </h5>
-<span class="text-muted text-left" style="display:block; margin: 0"><small>19 October 2016</small></span>
-<br><br>
-                <blockquote class="text-muted text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ipsum voluptates sapiente beatae non praesentium vitae dolorum qui, reprehenderit harum <br>
-<cite class="text-muted text-right text-bold">- Alex Martin</cite>
-</blockquote>
-                </div>
-</div>
-            </div><!-- end of right event <-->
-              <div class="clearfix"></div>
-            </-->
-          </div><!-- end of timeline events -->
-          <div class="clearfix"></div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </section>
-    }
-};
-
-
-
+      </section>
+      {/* end week block */}
+    </>
+  );
+}
