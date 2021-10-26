d40f255f775b3e51c3788ed5f1cfc83b06d9e535
AyaanaHassan <AyaanHassan2012@gmail.com>
2021-10-26 11:28:43 +0100

    timeline

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,14 +1,17 @@
 import NavBar from "../NavBar";
 import NameIcon from "../NameIcon";
+import css from "./Layout.module.css"
 
 const Layout = ({ children }) => {
   return (
-    <>
-      <NavBar>
+    <div class={css.layout}>
+      <NavBar/>
+      <main className={css.main}>
         <NameIcon />
         {children}
-      </NavBar>
-    </>
+        </main>
+    
+    </div>
   );
 };
 
---
+++ src/components/Timeline/index.js
@@ -0,0 +1,102 @@
+import React from "react";
+import css from "./Timeline.module.css"
+
+/* plan
+import react 
+import css 
+export default Timeline 
+props for timeline = jouneryData = week number, topic, resource 
+return = ul with jouneryData.map that will give us a contain (divs,h2, p)
+
+*/
+
+export default function Timeline(){
+    return {    
+
+
+  <section id="timeline-wrapper">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="timeline-top">
+          <div class="top-year">2018</span></div>
+
+        </div>
+        <div class="timeline-block">
+          <div class="timeline-events">
+            <br>
+            <div class="event l-event col-md-6 col-sm-6 col-xs-8 "><span class="thumb fa fa-codepen"></span>
+              <div class=" event-body">
+                <div class="person-image pull-left "><img src="https://cdn1.iconfinder.com/data/icons/user-pictures/100/male3-512.png" alt="person" /></div>
+<div class="event-content">
+<h5 class="text-primary text-left">IT-PLUS programming solution </h5>
+<span class="text-muted text-left" style="display:block; margin: 0"><small>19 October 2016</small></span>
+<br><br>
+                <blockquote class="text-muted text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ipsum voluptates sapiente beatae non praesentium vitae dolorum qui, reprehenderit harum <br>
+<cite class="text-muted text-right text-bold">- Alex Martin</cite>
+</blockquote>
+
+</div>
+              </div> <!-- end of event body -->
+<div class="clear-fix"></div>
+            </div><!-- end of left event -->
+            <div class="row"></div>
+            <div class="event r-event col-md-6 col-sm-6 col-xs-8 "><span class="thumb fa fa-facebook"></span>
+              <div class=" event-body">
+                <div class="person-image pull-left "><img src="https://cdn1.iconfinder.com/data/icons/user-pictures/100/male3-512.png" alt="person" /></div>
+<div class="event-content">
+<h5 class="text-primary text-left">IT-PLUS programming solution </h5>
+<span class="text-muted text-left" style="display:block; margin: 0"><small>19 October 2016</small></span>
+<br><br>
+                <blockquote class="text-muted text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ipsum voluptates sapiente beatae non praesentium vitae dolorum qui, reprehenderit harum <br>
+<cite class="text-muted text-right text-bold">- Alex Martin</cite>
+</blockquote>
+
+</div>
+              </div>
+            </div><!-- end of right event <-->
+              <div class="clearfix"></div>
+            </-->
+            <div class="row"></div>
+            <div class="event l-event col-md-6 col-sm-6 col-xs-8 "><span class="thumb fa fa-twitter"></span>
+              <div class=" event-body">
+                <div class="person-image pull-left "><img src="https://cdn1.iconfinder.com/data/icons/user-pictures/100/male3-512.png" alt="person" /></div>
+<div class="event-content">
+<h5 class="text-primary text-left">IT-PLUS programming solution </h5>
+<span class="text-muted text-left" style="display:block; margin: 0"><small>19 October 2016</small></span>
+<br><br>
+                <blockquote class="text-muted text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ipsum voluptates sapiente beatae non praesentium vitae dolorum qui, reprehenderit harum <br>
+<cite class="text-muted text-right text-bold">- Alex Martin</cite>
+</blockquote>
+                </div>
+</div>
+            </div><!-- end of left event <-->
+              <div class="clearfix"></div>
+            
+            <div class="row"></div>
+            <div class="event r-event col-md-6 col-sm-6 col-xs-8 ">
+              <span class="thumb fa fa-codepen"></span>
+             <div class=" event-body">
+                <div class="person-image pull-left "><img src="https://cdn1.iconfinder.com/data/icons/user-pictures/100/male3-512.png" alt="person" /></div>
+<div class="event-content">
+<h5 class="text-primary text-left">IT-PLUS programming solution </h5>
+<span class="text-muted text-left" style="display:block; margin: 0"><small>19 October 2016</small></span>
+<br><br>
+                <blockquote class="text-muted text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ipsum voluptates sapiente beatae non praesentium vitae dolorum qui, reprehenderit harum <br>
+<cite class="text-muted text-right text-bold">- Alex Martin</cite>
+</blockquote>
+                </div>
+</div>
+            </div><!-- end of right event <-->
+              <div class="clearfix"></div>
+            </-->
+          </div><!-- end of timeline events -->
+          <div class="clearfix"></div>
+        </div>
+      </div>
+    </div>
+  </section>
+    }
+};
+
+
+
