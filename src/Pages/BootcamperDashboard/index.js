f0707133e4842eaff154eb7910ed5e5bb5c4de66
valerio <romavalerioio@gmail.com>
2021-11-03 11:25:50 +0000

    current date displayed below the title

--- src/Pages/BootcamperDashboard/index.js
+++ src/Pages/BootcamperDashboard/index.js
@@ -6,6 +6,7 @@ export default function BootcamperDashboard({ Listings, name, recordings }) {
   return (
     <div>
       <h1 className={css.mainTitle}>Dashboard</h1>
+      <p className={css.subTitle}>{(new Date()).toDateString()}</p>
       <div className={css.dashboard}>
         <div className={`container`}>
           <h1 className={css.title}>Resource List</h1>
