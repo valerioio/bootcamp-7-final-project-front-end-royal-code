0dea0feebff6169ca20aa747024dc67b6534f506
C-L-D <clairedoswell1995@gmail.com>
2021-11-05 14:41:22 +0000

    Coach and bootcamper dashboard events using real data

--- src/Pages/BootcamperDashboard/index.js
+++ src/Pages/BootcamperDashboard/index.js
@@ -2,12 +2,27 @@ import css from "./BootcamperDashboard.module.css";
 // import Calendar from "../../components/Calendar";
 import PinBoard from "../../components/PinBoard";
 import Event from "../../components/Event";
-import { eventData } from "../../data";
+//import { eventData } from "../../data";
+import { useEffect, useRef, useState } from "react";
 function dateInTheFuture(daysFromToday) {
   return new Date(new Date().setDate(new Date().getDate() + daysFromToday));
 }
 
 export default function BootcamperDashboard({ Listings, name, recordings }) {
+  const [eventData, setEventData] = useState([]);
+
+  useEffect(() => {
+    async function getEventData() {
+      const res = await fetch(
+        "https://d27b2o3all.execute-api.eu-west-1.amazonaws.com/dev/events"
+      );
+      const data = await res.json();
+      setEventData(data);
+      return data;
+    }
+    getEventData();
+  }, []);
+
   return (
     <div>
       <h1 className={css.mainTitle}>Dashboard</h1>
