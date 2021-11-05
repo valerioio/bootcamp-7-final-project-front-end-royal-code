3feb059d0225a409b6e872ea5dd805fc4f276af2
C-L-D <clairedoswell1995@gmail.com>
2021-11-05 15:29:30 +0000

    Lecture Recordings page now using real data

--- src/Pages/LectureRecordingsPage/index.js
+++ src/Pages/LectureRecordingsPage/index.js
@@ -7,11 +7,28 @@
 // date
 // link
 
-import React from "react";
-import { recordings } from "../../data";
+import React, { useState, useEffect } from "react";
+//import { recordings } from "../../data";
 import css from "./LectureRecordingsPage.module.css";
 
 export default function LectureRecordingsPage() {
+  const [recordings, setRecordings] = useState([]);
+
+  useEffect(() => {
+    async function getEventData() {
+      const res = await fetch(
+        "https://d27b2o3all.execute-api.eu-west-1.amazonaws.com/dev/recordings"
+      );
+      const data = await res.json();
+      const sortedData = data.sort(function (a, b) {
+        return a.date.slice(0, 2) - b.date.slice(0, 2);
+      });
+      console.log(sortedData);
+      setRecordings(sortedData);
+      return data;
+    }
+    getEventData();
+  }, []);
   return (
     <>
       <h1 className={css.mainTitle}>Lecture Recordings</h1>
