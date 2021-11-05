2eb46f037a154fcdbc89abc066b35265d4c9aee5
AyaanaHassan <AyaanHassan2012@gmail.com>
2021-11-05 15:53:06 +0000

    gartic page changes

--- src/Pages/GarticPage/index.js
+++ src/Pages/GarticPage/index.js
@@ -14,37 +14,33 @@ import css from "./GarticPage.module.css";
 
 export default function GarticPage() {
   const gartic = energisers[0];
-  return ( 
-  <main className={css.body}>
-   <h1 className={css.mainTitle}>{gartic.name}</h1> <img className={css.garticLogo}
-        src={`${gartic.logo}`} alt="Gartic logo" />
-    <div className={css.content}>
-     
-     
+  return (
+    <main className={css.body}>
+      <header className={css.header}>
+        <h1 className={css.mainTitle}>{gartic.name}</h1>{" "}
+        <img className={css.garticLogo}
+          src={`${gartic.logo}`}
+          alt="Gartic logo"
+        />
+      </header>
+      <div className={css.content}>
        
-        <br/>
-        <br/>
-       
-        <br/> 
         <a className={css.link} href={`${gartic.link}`}>
           Play Gartic
         </a>
-        <br/>
+       
         <p className={css.garticDescription}>{gartic.description}</p>
-        <br/>
-        <br/>
-        <img
+        
+        <img className={css.garticImage}
           src={`${gartic.bootcamperImage}`}
           alt="Bootcampers playing Gartic"
         />
-        <br/>
-        <br/>
-        <img
+        
+        <img className={css.garticImage}
           src={`${gartic.screenshot}`}
           alt="Screenshot of Gartic being played"
         />
-    
-    </div> 
-     </main> 
+      </div>
+    </main>
   );
 }
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -97,7 +97,7 @@ function App() {
               <LoginPage />
             </Route>
             <Route exact path="/energisers/gartic">
-              <Layout user={user} color="#F3DE99">
+              <Layout user={user} >
                 <GarticPage />
               </Layout>
             </Route>
