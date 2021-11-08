116050e5d3061442c6a9177dd236ddde55f660cd
valerio <romavalerioio@gmail.com>
2021-11-08 19:39:08 +0000

    tested login as bootcamper or coach, and logout

--- cypress/integration/test1.spec.js
+++ cypress/integration/test1.spec.js
@@ -5,9 +5,14 @@ describe("test1", () => {
     cy.visit("/");
   });
   it("test1", () => {
-    cy.contains("Login").click();
+    cy.get("button").contains("Login").click();
     cy.get("#username").type("bootcamper@schoolofcode.co.uk");
     cy.get("#password").type("bootc4mper!");
+    cy.get("form").contains("Continue").click();
+    cy.contains("Log out").click();
+    cy.contains("Login").click();
+    cy.get("#username").type("coach@schoolofcode.co.uk");
+    cy.get("#password").type("co4ches!");
     cy.get("form").contains("Continue").click();
     cy.contains("Log out").click();
   });
