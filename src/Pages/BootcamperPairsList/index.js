f520b39b96e4885a742396984899690d6926a6d5
C-L-D <clairedoswell1995@gmail.com>
2021-11-09 11:42:30 +0000

    CSS copied to bootcamper pairs page. Some Avatars (n>19) render with black text, not sure why

--- src/Pages/BootcamperPairsList/index.js
+++ src/Pages/BootcamperPairsList/index.js
@@ -12,6 +12,63 @@ import React, { useReducer, useEffect, useState } from "react";
 //import { weeksOf2 } from "../../weeks-of-2";
 import css from "./BootcamperPairsList.module.css";
 import img1 from "../../Images/soc9.jpg";
+import { Avatar } from "@chakra-ui/react";
+
+let borderArr = [
+  "#FFADAD",
+  "#FFD6A5",
+  "#FDFFB6",
+  "#CAFFBF",
+  "#9BF6FF",
+  "#A0C4FF",
+  "#BDB2FF",
+  "#FFC6FF",
+  "#FFFFFC",
+  "#FFADAD",
+  "#FFD6A5",
+  "#FDFFB6",
+  "#CAFFBF",
+  "#9BF6FF",
+  "#A0C4FF",
+  "#BDB2FF",
+  "#FFC6FF",
+  "#FFFFFC",
+  "#FFADAD",
+  "#FFD6A5",
+  "#FDFFB6",
+  "#CAFFBF",
+  "#9BF6FF",
+  "#A0C4FF",
+  "#BDB2FF",
+  "#FFC6FF",
+  "#FFFFFC",
+];
+let avatarArr = [
+  "red.400",
+  "orange.400",
+  "green.400",
+  "cyan.400",
+  "blue.400",
+  "purple.400",
+  "pink.400",
+  "pink.200",
+  "red.400",
+  "orange.400",
+  "green.400",
+  "cyan.400",
+  "blue.400",
+  "purple.400",
+  "pink.400",
+  "pink.200",
+  "red.400",
+  "orange.400",
+  "green.400",
+  "cyan.400",
+  "blue.400",
+  "purple.400",
+  "pink.400",
+  "pink.200",
+];
 
 function reducer(week, action) {
   switch (action.type) {
@@ -45,30 +102,39 @@ export default function BootcamperPairList({ cohort }) {
 
   return (
     <>
-      <h1 className={css.mainTitle}>Cohort 7 Pairs</h1>
-      <button
-        className={css.button}
-        onClick={() => {
-          dispatch({ type: "decrement" });
-        }}
-      >
-        Last Week
-      </button>
-      <button
-        className={`${css.buttonRight} ${css.button}`}
-        onClick={() => {
-          dispatch({ type: "increment" });
-        }}
-      >
-        Next Week
-      </button>
-      <main className={css.main}>
+      <h1 className={css.mainTitle}>Cohort 7</h1>
+      <h2 className={css.secondaryTitle}>Pairs</h2>
+      <div className={css.alignment}>
+        <button
+          className={css.button}
+          onClick={() => {
+            dispatch({ type: "decrement" });
+          }}
+        >
+          Last Week
+        </button>
         <h2 className={css.secondaryTitle}>Week {week.count}</h2>
+        <button
+          className={`${css.buttonRight} ${css.button}`}
+          onClick={() => {
+            dispatch({ type: "increment" });
+          }}
+        >
+          Next Week
+        </button>
+      </div>
+      <main className={css.main}>
         <div className={css.content}>
           <ul className={css.bootcampersList}>
             {weeksOf2[week.count - 1].pairs.map((pair, i) => {
               return (
                 <li key={i + "043"} className={css.pairs}>
+                  <Avatar
+                    name={(i + 1).toString().split("").join(" ")}
+                    bg={avatarArr[i]}
+                    size="lg"
+                    className={css.avatar}
+                  />
                   {pair.map((bootcamper, j) => {
                     return (
                       <div key={i + "043" + j} className={css.bootcamper}>
@@ -84,7 +150,6 @@ export default function BootcamperPairList({ cohort }) {
               );
             })}
           </ul>
-
           <img className={css.image} src={img1} alt="Bootcampers" />
         </div>
       </main>
