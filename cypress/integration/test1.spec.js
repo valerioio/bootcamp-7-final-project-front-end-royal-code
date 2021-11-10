1f043014c1e63994715fbb32b0ee7b28e65959b5
Yrral <ykg.illani@gmail.com>
2021-11-10 22:48:01 +0000

    demo first bootcamper dashboard DONE drag and drop and and hover import

--- cypress/integration/test1.spec.js
+++ cypress/integration/test1.spec.js
@@ -1,19 +1,43 @@
 /// <reference types="cypress" />
+import "cypress-real-events";
+import '@4tw/cypress-drag-drop'
+// describe("test1", () => {
+//   beforeEach(() => {
+//     cy.visit("/");
+//   });
+//   it("test1", () => {
+//     cy.get("button").contains("Login").click();
+//     cy.get("#username").type("bootcamper@schoolofcode.co.uk");
+//     cy.get("#password").type("bootc4mper!");
+//     cy.get("form").contains("Continue").click();
+//     cy.contains("Log out").click();
+//     cy.contains("Login").click();
+//     cy.get("#username").type("coach@schoolofcode.co.uk");
+//     cy.get("#password").type("co4ches!");
+//     cy.get("form").contains("Continue").click();
+//     cy.contains("Log out").click();
+//   });
+// });
 
-describe("test1", () => {
+describe("Log in ", () => {
   beforeEach(() => {
     cy.visit("/");
   });
   it("test1", () => {
     cy.get("button").contains("Login").click();
-    cy.get("#username").type("bootcamper@schoolofcode.co.uk");
-    cy.get("#password").type("bootc4mper!");
-    cy.get("form").contains("Continue").click();
-    cy.contains("Log out").click();
-    cy.contains("Login").click();
-    cy.get("#username").type("coach@schoolofcode.co.uk");
-    cy.get("#password").type("co4ches!");
-    cy.get("form").contains("Continue").click();
-    cy.contains("Log out").click();
+    cy.wait(1500);
+    cy.wait(1000);
+    //  cy.get("#username").type("emanuel@email.com");
+    //  cy.get("#password").type("em4nuel!");
+    //  cy.wait(1000);
+    //  cy.get("form").contains("Continue").click();
+    cy.get("button").contains("+").realHover().wait(1500);
+    cy.get("button").contains("+").click().wait(1000);
+    cy.get("button").contains("+").click().wait(1000);
+    cy.get("button").contains("+").click().wait(1000);
+    cy.get("button").contains("x").realHover().wait(1500);
+    cy.get("button").contains("x").click().wait(1000);
+    cy.get(".PinBoard_pinBoard__3Ba5f>div:nth-child(5)").move({ deltaX: 100, deltaY: -1000 }).wait(1500);
+    cy.scrollTo('bottom').wait(1500);
   });
 });
