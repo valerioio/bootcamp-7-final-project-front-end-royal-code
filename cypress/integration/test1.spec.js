d3e440fc73fcacaf944734cad74b83623c01392c
valerio <romavalerioio@gmail.com>
2021-11-10 10:17:43 +0000

    added pauses

--- cypress/integration/test1.spec.js
+++ cypress/integration/test1.spec.js
@@ -6,14 +6,17 @@ describe("test1", () => {
   });
   it("test1", () => {
     cy.get("button").contains("Login").click();
+    cy.pause()
     cy.get("#username").type("bootcamper@schoolofcode.co.uk");
     cy.get("#password").type("bootc4mper!");
     cy.get("form").contains("Continue").click();
+    cy.pause()
     cy.contains("Log out").click();
     cy.contains("Login").click();
     cy.get("#username").type("coach@schoolofcode.co.uk");
     cy.get("#password").type("co4ches!");
     cy.get("form").contains("Continue").click();
+    cy.pause()
     cy.contains("Log out").click();
   });
 });
